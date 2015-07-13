e217e9f5528af49f304d365560ef2a58455469fd
Robert Morgan <robbie.morgan@gmail.com>
2015-07-13 16:01:19 +0100

    refactoring

--- app/app.rb
+++ app/app.rb
@@ -2,35 +2,42 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
 
-get '/' do
-	erb :'users/welcome_page'
-end
+  enable :sessions
+  set :session_secret, 'super secret'
 
-post '/user' do 
-  @user = User.create(name: params[:name], 
-              email: params[:email], 
-              username: params[:username],
-              password: params[:password])
-  if @user.save
-    @current_user = @user.username
-    redirect to('/user')
-  else
-    @user.errors.each do |e|
-      puts e
-    end
+  get '/' do
+    erb :'users/welcome_page'
+  end
+
+  post '/user' do
+    session[:user] = params[:username]
+    @current_user = session[:user]
     erb :'users/user'
   end
-end
-  
-  get '/user' do 
+
+  get '/user' do
     "Success!"
     erb :'users/user'
   end
-	
 
-post '/sign_up' do 
-  erb :'users/sign_up'	
-end
+  post '/sign_up' do
+    erb :'users/sign_up'
+  end
 
+  post '/user/new' do
+    @user = User.create(name: params[:name],
+                        email: params[:email],
+                        username: params[:username],
+                        password: params[:password])
+    if @user.save
+      session[:user] = @user.username
+      @current_user = session[:user]
+      redirect to('/user')
+    else
+      @user.errors.each do |e|
+        puts e
+      end
 
+    end
+  end
 end
